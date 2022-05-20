31ce4094e7842176d32e27723f27385f53f22009
apenot <antoine.penot@epitech.eu>
2022-05-20 19:04:59 +0200

    MODIFIED: Texture class is working now

--- src/Texture2D.cpp
+++ src/Texture2D.cpp
@@ -5,32 +5,79 @@
 ** Texture2D
 */
 
-#include "Texture2D.hpp"
+
 #include <raylib.h>
+#include "Texture2D.hpp"
 
 namespace indie {
 
-Texture::Texture()
+Texture::Texture(const std::string &fileName)
 {
+    _texture = std::make_unique<Texture2D>(::LoadTexture(fileName.c_str()));
+    _isLoaded = true;
 }
 
 Texture::~Texture()
 {
+    if (_isLoaded)
+        UnloadTexture();
 }
 
 void Texture::LoadTexture(const std::string &fileName)
 {
-    _texture = ::LoadTexture(fileName.c_str());
+    if (_isLoaded)
+        UnloadTexture();
+    _texture = std::make_unique<Texture2D>(::LoadTexture(fileName.c_str()));
+    _isLoaded = true;
 }
 
 void Texture::UnloadTexture()
 {
-    ::UnloadTexture(_texture);
+    if (_isLoaded) {
+        ::UnloadTexture(*(_texture.get()));
+        _isLoaded = false;
+    }
 }
 
-void Texture::DrawTexture(int posX, int posY, Color tint)
+int Texture::getWidth() const
 {
-    ::DrawTexture(_texture, posX, posY, tint);
+    return _texture->width;
+}
+
+int Texture::getHeight() const
+{
+    return _texture->height;
+}
+
+void Texture::DrawTexture(int posX, int posY, Color tint = WHITE)
+{
+    if (_isLoaded)
+        ::DrawTexture(*(_texture.get()), posX, posY, tint);
+}
+
+void Texture::DrawTexture(int posX, int posY)
+{
+    if (_isLoaded)
+        ::DrawTexture(*(_texture.get()), posX, posY, WHITE);
+}
+
+void Texture::DrawTextureRec(Rectangle source, Vector2 position, Color tint = WHITE)
+{
+    if (_isLoaded)
+        ::DrawTextureRec(*(_texture.get()), source, position, tint);
+}
+
+void Texture::DrawTextureRec(Rectangle source, Vector2 position)
+{
+    if (_isLoaded)
+        ::DrawTextureRec(*(_texture.get()), source, position, WHITE);
+}
+
+void Texture::operator=(const Texture2D &other)
+{
+    if (_isLoaded)
+        UnloadTexture();
+    _texture = std::make_unique<Texture2D>(other);
 }
 
 }
--- src/Texture2D.hpp
+++ src/Texture2D.hpp
@@ -9,19 +9,80 @@
 #define TEXTURE2D_HPP_
 
 #include <iostream>
+#include <memory>
+
+struct Texture;
+struct Rectangle;
+struct Vector2;
+struct Color;
+
+typedef Texture Texture2D;
 
 namespace indie {
     class Texture {
         public:
-            Texture();
+            Texture(const std::string &fileName);
             ~Texture();
 
+            /**
+             * @brief Load texture from file into GPU memory (VRAM)
+             * @param fileName The file name
+             */
             void LoadTexture(const std::string &fileName);
+
+            /**
+             * @brief Unload texture from GPU memory (VRAM)
+             */
             void UnloadTexture();
+
+            /**
+             * @brief Return the width of the loaded texture
+             */
+            int getWidth() const;
+
+            /**
+             * @brief Return the height of the loaded texture
+             */
+            int getHeight() const;
+
+            /**
+             * @brief Draw a Texture2D
+             * @param posX The horizontal position
+             * @param posY The vertical position
+             * @param tint The color tint
+             */
             void DrawTexture(int posX, int posY, Color tint);
+
+            /**
+             * @brief Draw a Texture2D
+             * @param posX The horizontal position
+             * @param posY The vertical position
+             */
+            void DrawTexture(int posX, int posY);
+
+            /**
+             * @brief Draw a part of a texture defined by a rectangle
+             * @param source The rectangle defining the part of the texture to draw
+             * @param position The position to draw the texture
+             * @param tint The color tint
+             */
+            void DrawTextureRec(Rectangle source, Vector2 position, Color tint);
+
+            /**
+             * @brief Draw a part of a texture defined by a rectangle
+             * @param source The rectangle defining the part of the texture to draw
+             * @param position The position to draw the texture
+             */
+            void DrawTextureRec(Rectangle source, Vector2 position);
+
+            /**
+             * @brief Copy a texture
+             */
+            void operator=(const Texture2D &other);
         protected:
         private:
-            Texture2D _texture;
+            std::unique_ptr<Texture2D> _texture;
+            bool _isLoaded;
     };
 }
 #endif /* !TEXTURE2D_HPP_ */
