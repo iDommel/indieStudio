6a7328759372f003700a6043f33b10b79f450394
Mfolio2004 <maxence.folio@epitech.eu>
2022-05-24 10:52:24 +0200

    [ADD] documentation

--- include/shape3d.hpp
+++ include/shape3d.hpp
@@ -1,14 +1,44 @@
+#ifndef SHAPE3DH
+    #define SHAPE3DH
 
-struct Vector3;
-struct Color;
+    struct Vector3;
+    struct Color;
 
-class shape3D {
-    public:
-        shape3D();
-        ~shape3D();
+    /**struct Shape3D;
+     * @brief Shape3D object
+     */ 
+    class shape3D {
+        public:
+            /**
+             * @brief Construct a new shape3D object
+             */
+            shape3D();
+            /**
+             * @brief Destroy shape3D object
+             */
+            ~shape3D();
 
-        void DrawSphere(Vector3 centerPos, float radius, Color color);
-        void DrawCube(Vector3 position, Vector3 size, Color color);
-        void DrawGrid(int slices, float spacing);
+            /**
+             * @brief Draw sphere 
+             * @param centerPos position of the center of the sphere
+             * @param radius raduis of the sphere
+             * @param color Color of the sphere
+             */
+            void DrawSphere(Vector3 centerPos, float radius, Color color);
+            /**
+             * @brief Draw cube 
+             * @param position position of the center of the sphere
+             * @param size size {x, y, z} of the cube
+             * @param color Color of the cube
+             */
+            void DrawCube(Vector3 position, Vector3 size, Color color);
+            /**
+             * @brief Draw grid 
+             * @param slices size of the grid 
+             * @param spacing size of one slice 
+             */
+            void DrawGrid(int slices, float spacing);
 
-};
+    };
+
+#endif /* !MODELH */
