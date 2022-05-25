b8a3ca11427908d4c23c357a76cb421c03673dad
apenot <antoine.penot@epitech.eu>
2022-05-25 13:38:00 +0200

    ADDED: Font Class

---
+++ src/Font.hpp
@@ -0,0 +1,44 @@
+/*
+** EPITECH PROJECT, 2022
+** indieStudio
+** File description:
+** Font
+*/
+
+#ifndef FONT_HPP_
+#define FONT_HPP_
+
+#include <memory>
+
+struct Font;
+
+namespace indie {
+class Font {
+    public:
+        Font();
+        ~Font();
+
+        /**
+         * @brief Get the default Font
+         */
+        ::Font GetFontDefault(void) const;
+
+        /**
+         * @brief Load font from file into GPU memory (VRAM)
+         * @param fileName Path to font file
+         */
+        void LoadFont(const std::string &fileName);
+
+        /**
+         * @brief Unload Font from GPU memory (VRAM)
+         */
+        void UnloadFont(void);
+
+    protected:
+    private:
+        std::unique_ptr<::Font> _font;
+        bool _isLoaded;
+};
+}
+
+#endif /* !FONT_HPP_ */
