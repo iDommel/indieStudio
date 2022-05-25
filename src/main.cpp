f6013f0fb5104f55a89c2175d91bd3eae8e7e026
apenot <antoine.penot@epitech.eu>
2022-05-25 11:24:30 +0200

    ADDED: test functions and textures in separated directories

--- src/main.cpp
+++ src/main.cpp
@@ -6,50 +6,10 @@
 */
 #include <exception>
 #include <iostream>
-#include <raylib.h>
-#include "Texture2D.hpp"
+
+#include "raylib.h"
 
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-
-    InitWindow(screenWidth, screenHeight, "raylib [textures] example - texture loading and drawing");
-
-    // NOTE: Textures MUST be loaded after Window initialization (OpenGL context is required)
-    indie::Texture texture("/home/apenot/Tek2re/B-YEP-400/indieStudio/src/test.png"); // Texture loading
-    //---------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose()) // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        texture.DrawTexture(screenWidth / 2 - texture.getWidth() / 2, screenHeight / 2 - texture.getHeight() / 2, WHITE);
-
-        DrawText("this IS a texture!", 360, 370, 10, GRAY);
-
-        EndDrawing();
-        //----------------------------------------------------------------------------------
-    }
-
-    // De-Initialization
-    texture.UnloadTexture(); // Texture unloading
-    //--------------------------------------------------------------------------------------
-
-    CloseWindow(); // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
     return 0;
 }
