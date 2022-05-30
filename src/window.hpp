0fa0bcd8089d7c88b1e6736f4bc9fe396f048e07
leo maman <leo.maman@epitech.eu>
2022-05-30 23:16:59 +0200

    UPDATED: adding control event, need to add further keyevent

--- src/window.hpp
+++ src/window.hpp
@@ -46,10 +46,38 @@ namespace indie {
         * @param camera
         */
         void beginMode2D(Camera2D camera);
-        // @brief End 3D mode.
+        /// @brief End 3D mode.
         void endMode3D();
-        // @brief End 2D mode.
+        /// @brief End 2D mode.
         void endMode2D();
+        /// @brief Check if a mouse button has been pressed once
+        bool isMouseButtonPressed(int button);
+        /// @brief Check if a mouse button is being pressed
+        bool isMouseButtonDown(int button);
+        /// @brief Check if a mouse button has been released once
+        bool isMouseButtonReleased(int button);
+        /// @brief Check if a mouse button is NOT being pressed
+        bool isMouseButtonUp(int button);
+        /// @brief Check if a gamepad is available
+        bool isGamepadAvailable(int gamepad);
+        /// @brief  Get gamepad internal name id
+        const char* getGamepadName(int gamepad);
+        /// @brief  Check if a gamepad button has been pressed once
+        bool isGamepadButtonPressed(int gamepad, int button);
+        /// @brief  Check if a gamepad button is being pressed
+        bool isGamepadButtonDown(int gamepad, int button);
+        /// @brief  Check if a gamepad button has been released once
+        bool isGamepadButtonReleased(int gamepad, int button);
+        /// @brief  Check if a gamepad button is NOT being pressed
+        bool isGamepadButtonUp(int gamepad, int button);
+        /// @brief  Get the last gamepad button pressed
+        int getGamepadButtonPressed(void);
+        /// @brief  Get gamepad axis count for a gamepad
+        int getGamepadAxisCount(int gamepad);
+        /// @brief  Get axis movement value for a gamepad axis
+        float getGamepadAxisMovement(int gamepad, int axis);  
+        /// @brief Set internal gamepad mappings (SDL_GameControllerDB)
+        int setGamepadMappings(const char* mappings);
     };
 }
 
