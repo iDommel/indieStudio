ba8d8e4a2447271ab25a7cf8cfb4f9e6c858aaa9
Mfolio2004 <maxence.folio@epitech.eu>
2022-06-08 15:14:41 +0200

    DELETED: comments in cpp

--- test_functions/TestModelAnimation.cpp
+++ test_functions/TestModelAnimation.cpp
@@ -3,56 +3,43 @@
 
 void test_raylib_modelAnimation()
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
 
     InitWindow(screenWidth, screenHeight, "raylib [models] example - model animation");
 
-    // Define the camera to look into our 3d world
     Camera camera = { 0 };
-    camera.position = (Vector3){ 10.0f, 10.0f, 10.0f }; // Camera position
-    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
-    camera.fovy = 45.0f;                                // Camera field-of-view Y
-    camera.projection = CAMERA_PERSPECTIVE;             // Camera mode type
+    camera.position = (Vector3){ 10.0f, 10.0f, 10.0f }; 
+    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f }; 
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };        
+    camera.fovy = 45.0f;                               
+    camera.projection = CAMERA_PERSPECTIVE;            
 
-    Model model = LoadModel("assets_test/guy.iqm");                    // Load the animated model mesh and basic data
-    Texture2D texture = LoadTexture("assets_test/guytex.png");         // Load model texture and set material
-    SetMaterialTexture(&model.materials[0], MATERIAL_MAP_DIFFUSE, texture);     // Set model material map texture
+    Model model = LoadModel("assets_test/guy.iqm");            
+    Texture2D texture = LoadTexture("assets_test/guytex.png");       
+    SetMaterialTexture(&model.materials[0], MATERIAL_MAP_DIFFUSE, texture);   
 
-    Vector3 position = { 0.0f, 0.0f, 0.0f };            // Set model position
-
-    // Load animation data
+    Vector3 position = { 0.0f, 0.0f, 0.0f };      
 
     unsigned int animsCount = 0;
     indie::ModelAnimation anim("assets_test/guyanim.iqm", &animsCount);
     int animFrameCounter = 0;
 
-    SetCameraMode(camera, CAMERA_FREE); // Set free camera mode
+    SetCameraMode(camera, CAMERA_FREE); 
 
-    SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    SetTargetFPS(60);
 
-    // Main game loop
-    while (!WindowShouldClose())        // Detect window close button or ESC key
+    while (!WindowShouldClose())       
     {
-        // Update
-        //----------------------------------------------------------------------------------
         UpdateCamera(&camera);
 
-        // Play animation when spacebar is held down
         if (IsKeyDown(KEY_SPACE))
         {
             animFrameCounter++;
             anim.updateModelAnimation(model, animFrameCounter);
             if (animFrameCounter >= anim.getFrameCount()) animFrameCounter = 0;
         }
-        //----------------------------------------------------------------------------------
 
-        // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
@@ -61,28 +48,17 @@ void test_raylib_modelAnimation()
 
                 DrawModelEx(model, position, (Vector3){ 1.0f, 0.0f, 0.0f }, -90.0f, (Vector3){ 1.0f, 1.0f, 1.0f }, WHITE);
 
-                // for (int i = 0; i < model.boneCount; i++)
-                // {
-                //     DrawCube(anims[0].framePoses[animFrameCounter][i].translation, 0.2f, 0.2f, 0.2f, RED);
-                // }
-
-                DrawGrid(10, 1.0f);         // Draw a grid
+                DrawGrid(10, 1.0f);
 
             EndMode3D();
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
+    UnloadTexture(texture); 
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    UnloadTexture(texture);     // Unload texture
-
-    // Unload model animations data
     anim.unloadModelAnimation();
-    // RL_FREE(anims);
 
-    UnloadModel(model);         // Unload model
+    UnloadModel(model); 
 
-    CloseWindow();              // Close window and OpenGL context
+    CloseWindow();  
 }
