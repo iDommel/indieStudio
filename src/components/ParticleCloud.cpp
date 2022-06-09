dbee1842d32aeb9e191f9cf9434308c7aaca5f74
Zac <izaac.carcenac-sautron@epitech.eu>
2022-06-09 16:48:53 +0200

    WIP: bezier curve points choosing

--- src/components/ParticleCloud.cpp
+++ src/components/ParticleCloud.cpp
@@ -5,11 +5,30 @@
 ** ParticleCloud
 */
 
+#include "raylib.h"
 #include "ParticleCloud.hpp"
 
 namespace indie {
-ParticleCloud::ParticleCloud()
+ParticleCloud::ParticleCloud(Vector3 start, Vector3 end, double nbParticles,
+double curvature, double dispersion, float lifeTime):
+_timeToLive(lifeTime)
 {
+    float x = 0;
+    float y = 0;
+    float z = 0;
+
+    _bezierCurves.resize(nbParticles);
+    for (double i = 0; i < nbParticles; i++) {
+        _bezierCurves[i].resize(curvature + 2);
+        _bezierCurves[i][0] = start;
+        for (double j = 1; j < curvature + 1; j++) {
+            x = random(start.x, end.x);
+            y = random(start.y, end.y);
+            z = random(start.z, end.z);
+            _bezierCurves[i][j] = Vector3(x, y, z);
+        }
+        _bezierCurves[i][curvature + 2] = end;
+    }
 }
 
 ParticleCloud::~ParticleCloud()
--- src/components/ParticleCloud.hpp
+++ src/components/ParticleCloud.hpp
@@ -12,10 +12,20 @@
 #include "Position.hpp"
 
 #include <vector>
+#include <random>
 
 struct Vector3;
 
 namespace indie {
+
+template<typename T>
+T random(T range_from, T range_to) {
+    std::random_device                  rand_dev;
+    std::mt19937                        generator(rand_dev());
+    std::uniform_int_distribution<T>    distr(range_from, range_to);
+    return distr(generator);
+}
+
 class ParticleCloud : public Component {
     public:
         /**
@@ -23,11 +33,11 @@ class ParticleCloud : public Component {
          * @param start The start position of the cloud
          * @param end The end position of the cloud
          * @param nbParticles The number of particles in the cloud
-         * @param curvature The number of bend of each particule
-         * @param dispersion The dispersion of the cloud
-         * 
+         * @param curvature The number of bend of each particule (0=straight, 1=curve, 2+=curves)
+         * @param dispersion The dispersion of the cloud, like a radius around the followed curve
+         * @param lifeTime The lifetime of the cloud, each particule will reach the end vector after this time
         **/
-        ParticleCloud(Vector3 start, Vector3 end, int nbParticles, int curvature, int dispersion, float lifeTime);
+        ParticleCloud(Vector3 start, Vector3 end, double nbParticles, double curvature, double dispersion, float lifeTime);
         ~ParticleCloud();
 
     protected:
